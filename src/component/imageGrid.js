a610d9aa8f6e66c1f2ccb782570256cf9ea94a4b
Rahul Krishna <57654450+rkrishnavedic@users.noreply.github.com>
2021-04-11 23:26:28 +0530

    feat:

--- src/component/imageGrid.js
+++ src/component/imageGrid.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import useFirestore from '../hooks/useFirestore';
 import '../App.css';
 import {motion} from 'framer-motion';
-import heart from 'bootstrap-icons/icons/heart.svg';
 import heartFill from 'bootstrap-icons/icons/heart-fill.svg';
+import heart from 'bootstrap-icons/icons/heart.svg';
+import penIcon from 'bootstrap-icons/icons/pencil-square.svg';
 import trash from 'bootstrap-icons/icons/trash.svg';
 import DeletePop from './delete';
 import FavClick from './favclick';
@@ -30,9 +31,17 @@ const ImageGrid = ({setSelectedImg})=>{
                         <motion.div className="justify-content-around card-text">
                         <motion.img initial={{scale:1}} onClick={()=>FavClick(doc.id, doc.favorite)} src={hearticon(doc.favorite)} />
                         <motion.img style={{float:'right'}} initial={{scale:0.8}} animate={{scale:1}} src={trash} onClick={()=>setDeleteId(doc.id)} />
+                        
+                        </motion.div>
+                        <hr/>
+                        <motion.div style={{fontSize:'0.7rem'}} className="card-text">
+                        <img src={penIcon} alt="edit icon"/>
+                          &nbsp;  
+                            Want to add some story?
+                            (working over this feature!)
                         </motion.div>
                     </div>
-
+                    
                 </motion.div>
                 )
             })}
